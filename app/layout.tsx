fae50ead5d800ffc13f0d93b016543e4f46c79b0
Basilisk <prith.tiu13@gmail.com>
2024-01-19 12:07:31 +0530

    needs updation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,35 @@
-import type { Metadata } from 'next'
-import { Figtree } from 'next/font/google'
-import './globals.css'
-import Sidebar from '@/components/Sidebar'
-import SupabaseProvider from '@/providers/SupabaseProvider'
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+import Sidebar from "@/components/Sidebar";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+import ToasterProvider from "@/providers/ToasterProvider";
 
-const font = Figtree({ subsets: ['latin'] })
+const font = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Spotify Clone',
-  description: 'Listen To Music!',
-}
+  title: "Spotify Clone",
+  description: "Listen To Music!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
         <SupabaseProvider>
-        <Sidebar>
-          {children}
-          </Sidebar>
+          <UserProvider>
+            <ModalProvider products={products} />
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
         </SupabaseProvider>
       </body>
     </html>
-  )
+  );
 }
