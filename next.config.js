d8e2ea7e64b5b76af2aea8661fd417287f0bd43c
Basilisk <prith.tiu13@gmail.com>
2024-01-24 10:49:38 +0530

    Fully Functional upto now

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: [pehrkfnjdlnzvjlassfd.supabase.co],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
